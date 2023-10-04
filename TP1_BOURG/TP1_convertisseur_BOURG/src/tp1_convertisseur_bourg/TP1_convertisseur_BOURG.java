a786bc5ce0d2472368eec348e20f9bb156f521a1
emilie bourg <emilie.bourg@epfedu.fr>
2023-10-04 14:35:19 +0200

    Emilie Bourg TP1 partie 2, création de convertisseur de temperature avec des methodes

--- TP1_BOURG/TP1_convertisseur_BOURG/src/tp1_convertisseur_bourg/TP1_convertisseur_BOURG.java
+++ TP1_BOURG/TP1_convertisseur_BOURG/src/tp1_convertisseur_bourg/TP1_convertisseur_BOURG.java
@@ -18,15 +18,38 @@ public class TP1_convertisseur_BOURG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    double celsius;
-    double kelvin;
+    double temp;
+    int choix;
     Scanner sc = new Scanner(System.in);
-    System.out.println("Saisissez une temperature en C que vous voulez convertir en Kelvin");
-    celsius=sc.nextDouble();
+    System.out.println("Saisissez une temperature que vous voulez convertir dans une autre unite");
+    temp=sc.nextDouble();
     //kelvin = celsius + 273.15;
-    kelvin = CelciusVersKelvin(celsius);
-    System.out.println("Pour "+celsius+" C, la temperature vaut "+kelvin+" K");
-    
+    System.out.println("""
+                       Saisissez la conversion que vous souhaiter effectuer : 
+                       1 pour Celcius vers Kelvin 
+                       2 pour Kelvin vers Celcius 
+                       3 pour Farenheit vers Celcius 
+                       4 pour Celcius vers Farenheit 
+                       5 pour Kelvin vers Farenheit 
+                       6 pour Farenheit vers Kelvin""");
+    choix=sc.nextInt();
+    while (choix!=1 && choix!=2 && choix!=3 && choix!=4 && choix!=5 && choix!=6){
+        System.out.println("le chiffre entre ne correspond à aucune posibilite, veuillez en entrer un autre");
+        choix=sc.nextInt();
+    }
+    if (choix==1){
+        System.out.println("Pour "+temp+" C, la temperature vaut "+CelciusVersKelvin(temp)+" K");
+    } else if (choix==2){
+        System.out.println("Pour "+temp+" K, la temperature vaut "+KelvinVersCelcius(temp)+" C");
+    } else if (choix==3){
+        System.out.println("Pour "+temp+" F, la temperature vaut "+FarenheitVersCelcius(temp)+" C");
+    } else if (choix==4){
+        System.out.println("Pour "+temp+" C, la temperature vaut "+CelciusVersFarenheit(temp)+" F");
+    } else if (choix==5){
+         System.out.println("Pour "+temp+" K, la temperature vaut "+KelvinVersFarenheit(temp)+" F");
+    } else if (choix==6){
+        System.out.println("Pour "+temp+" F, la temperature vaut "+FarenheitVersKelvin(temp)+" K");
+    }
     }
     
     public static double CelciusVersKelvin (double tCelcius) {
