ae424eaef9ec5e8ab027059643721f2dcaf575f1
emilie bourg <emilie.bourg@epfedu.fr>
2023-10-05 21:58:38 +0200

    Emilie Bourg TP1 partie 3, jeu trouver le nombre

--- TP1_BOURG/TP1_guessMyNumber_BOURG/src/tp1_guessmynumber_bourg/TP1_guessMyNumber_BOURG.java
+++ TP1_BOURG/TP1_guessMyNumber_BOURG/src/tp1_guessmynumber_bourg/TP1_guessMyNumber_BOURG.java
@@ -35,6 +35,22 @@ public class TP1_guessMyNumber_BOURG {
             result=modefacile(nombre,n1);
             System.out.println("Vous avez gagne en " + result + " coups");
         }
+        else if(choixmode==2){
+            result=modemoyen(nombre,n1);
+            if (result!=10){
+                System.out.println("Vous avez gagne en "+result+" coups");
+            } else if(result==10){
+                System.out.println("Perdu! Vous avez utilise les 10 coups");
+            }
+        }
+        else if (choixmode==3){
+            result=modedifficile(nombre,n1);
+            if (result!=5){
+                System.out.println("Vous avez gagne en "+result+" coups");
+            } else if(result==5){
+                System.out.println("Perdu! vous avez utilise les 5 coups");
+            }
+        }
     }
 
     public static int modefacile(int nombrej, int nombreo) {
@@ -68,7 +84,7 @@ public class TP1_guessMyNumber_BOURG {
                 i += 1;
             }
         } if (i==10){
-            return 2;
+            return 10;
         }
         return i;
     }
@@ -76,7 +92,7 @@ public class TP1_guessMyNumber_BOURG {
     public static int modedifficile(int nombrej, int nombreo) {
         Scanner sc = new Scanner(System.in);
         int i = 1;
-        while (nombreo != nombrej) {
+        while (nombreo != nombrej && i!=5) {
             if (nombreo < nombrej) {
                 System.out.println("trop grand, choisir un autre nombre:");
                 nombrej = sc.nextInt();
@@ -86,6 +102,8 @@ public class TP1_guessMyNumber_BOURG {
                 nombrej = sc.nextInt();
                 i += 1;
             }
+        } if (i==5){
+            return 5;
         }
         return i;
     }
