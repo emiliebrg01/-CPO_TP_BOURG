ec637b7eac90d7a125a519130b9e9b21934a2202
emilie bourg <emilie.bourg@epfedu.fr>
2023-10-06 08:26:17 +0200

    Emilie Bourg TP1 partie 3, programme qui compte le nombre de face du dé qui sont tombées et affiche le pourcentage de chaque face

--- TP1_BOURG/TP1_stats_BOURG/src/tp1_stats_bourg/TP1_stats_BOURG.java
+++ TP1_BOURG/TP1_stats_BOURG/src/tp1_stats_bourg/TP1_stats_BOURG.java
@@ -22,26 +22,31 @@ public class TP1_stats_BOURG {
     int face[]=new int[6];
     Scanner sc = new Scanner(System.in);
     int m;
-    System.out.println("Entrez un nombre entier");
+    System.out.println("Entrez le nombre de lancer que vous voulez faire");
     m=sc.nextInt();
     
     for (int i=0; i<m ; i++){
         Random alea = new Random();
         int lancer = alea.nextInt(6);
         System.out.println(lancer);
-        if (lancer==1){
+        if (lancer==0){
             face[0]+=1;
-        } else if(lancer==2){
+        } else if(lancer==1){
             face[1]+=1;
-        } else if(lancer==3){
+        } else if(lancer==2){
             face[2]+=1;
-        } else if(lancer==4){
+        } else if(lancer==3){
             face[3]+=1;
-        } else if(lancer==5){
+        } else if(lancer==4){
             face[4]+=1;
-        } else if(lancer==6){
+        } else if(lancer==5){
             face[5]+=1;
         }
+    }
+    for(int i=0; i<6; i++){
+        double pourcent = 0;
+        pourcent= face[i]*100/m;
+        System.out.println("Le pourcentage de face "+ (i+1) +" obtenue est: "+pourcent+"%");
     }
     }
     
