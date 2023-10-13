aab8fbc0281daee13a94393cdd56739c5ba26d67
emilie bourg <emilie.bourg@epfedu.fr>
2023-10-13 12:01:01 +0200

    Emilie Bourg TP2 partie 1, créer objet et manipuler éléments, utiliser class méthode et attribut avec bouteille de Biere

--- TP2_BOURG/TP2_Bieres_BOURG/src/tp2_bieres_bourg/BouteilleBiere.java
+++ TP2_BOURG/TP2_Bieres_BOURG/src/tp2_bieres_bourg/BouteilleBiere.java
@@ -1,8 +1,8 @@
 /*
  *Emilie Bourg
  *TDC
- * 
- * 10 octobre 2023
+ *Manipuler les class et les objets 
+ *10 octobre 2023
  */
 package tp2_bieres_bourg;
 
@@ -11,5 +11,37 @@ package tp2_bieres_bourg;
  * @author Emilie
  */
 public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+    public void lireEtiquette(){
+        System.out.println("Bouteille de "+nom+" ("+degreAlcool+" degres) \nBrasserie : "+brasserie);
+    }
+    public boolean Décapsuler(){
+        if (ouverte ==false){
+            ouverte = true;
+            return true;
+        } else{
+            System.out.println("erreur : biere déjà ouverte");
+            return false;
+        }
+    }
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie){ 
+        nom = unNom; 
+        degreAlcool = unDegre; 
+        brasserie = uneBrasserie; 
+        ouverte = false; 
+    }
     
+    @Override 
+    public String toString() { 
+        String chaine_a_retourner; 
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés)"
+                + " Ouverte ? "; 
+        if (ouverte == true ) chaine_a_retourner += "oui" ; 
+        else chaine_a_retourner += "non" ; 
+        return chaine_a_retourner ; }
 }
+
