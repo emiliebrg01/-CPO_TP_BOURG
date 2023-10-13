cf4a07b30ce12e3409fc88d1212e4ed094e6a075
emilie bourg <emilie.bourg@epfedu.fr>
2023-10-13 14:53:11 +0200

    Emilie Bourg TP2 partie 3, créer objet et manipuler éléments. Utilisation avec plats et nombre de calories associer

---
+++ TP2_BOURG/TP2_manip_Bourg/src/tp2_manip_bourg/Tartiflette.java
@@ -0,0 +1,19 @@
+/*
+ * Emilie Bourg
+ * TDC
+ * 13/10/2023
+ * 
+ */
+package tp2_manip_bourg;
+
+/**
+ *
+ * @author Emilie
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette(int calories){
+        nbCalories=calories;
+    }
+}
