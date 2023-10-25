cc082ace0a08a7df1272300eda2a37029b3493b2
emilie bourg <emilie.bourg@epfedu.fr>
2023-10-25 12:39:52 +0200

    Emilie Bourg Mini Projet 1, Création de la classe CelluleLumineuse permettant de créer et gérer une cellule lumineuse, test dans main assurant le fonctionnement de la calsse

---
+++ LightOff_ Bourg _version_console/src/lightoff_/bourg/_version_console/LightOff_Bourg_version_console.java
@@ -0,0 +1,28 @@
+/*
+ * Emilie Bourg
+ * 25/10/2023
+ * TDC
+ * 
+ */
+package lightoff_.bourg._version_console;
+
+/**
+ *
+ * @author Emilie
+ */
+public class LightOff_Bourg_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CelluleLumineuse la1 = new CelluleLumineuse ();
+        CelluleLumineuse la2 = new CelluleLumineuse ();
+        CelluleLumineuse la3 = new CelluleLumineuse ();
+        
+        la2.activerCellule();
+        
+        System.out.println(la1+" "+la2+" "+la3);
+    }
+    
+}
